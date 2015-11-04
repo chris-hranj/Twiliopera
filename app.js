d624a5bbcdf72136c9c6cbca5bbded8b9efe51a9
faiq <faiqrazarizvi@gmail.com>
2015-11-04 00:11:22 -0500

    clean up useless comments and files

--- app.js
+++ app.js
@@ -11,12 +11,9 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,10 +29,6 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// error handlers:
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -46,8 +39,6 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
--- yo2.js
+++
@@ -1,8 +0,0 @@
-//use for testing the midigen.js functionality.
-
-var mg = require('./midigen.js');
-
-mg('123456789123456789', function(err){
-	if(err)
-		console.log(err);
-})
